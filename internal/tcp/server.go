52e38b938a1d475c85403ce4a6979fd0918e2c3e
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-08-16 22:48:39 +0300

    feat: implement tcp server as transport layer

---
+++ internal/tcp/server.go
@@ -0,0 +1,60 @@
+package tcp
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"nova/pkg/resp"
+)
+
+type Handler interface {
+	Serve([]string) []byte
+}
+
+type Server struct {
+	Addr    string
+	Handler Handler
+}
+
+func (s *Server) ListenAndServe() {
+	ln, err := net.Listen("tcp", s.Addr)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("failed to accept connection: %v\n", err.Error())
+			continue
+		}
+
+		fmt.Println("got new connection")
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	for {
+		buff := make([]byte, 100) // TODO: what size i need and where should i put it?
+		n, err := conn.Read(buff)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("connection closed. Maybe...")
+				break
+			}
+
+			fmt.Printf("failed to read request due to: %v", err.Error())
+		}
+
+		cmd, _ := resp.Decode(buff[:n])
+		resp := s.Handler.Serve(cmd)
+		fmt.Println("response:", string(resp))
+
+		n, err = conn.Write(resp)
+		fmt.Println(n, err)
+
+		fmt.Println(cmd)
+	}
+}
