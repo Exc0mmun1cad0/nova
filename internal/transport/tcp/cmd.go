4c53b1bd2d2c03ef6f4c0aa75485857e65560856
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-08-16 21:56:11 +0300

    feat: implement basic commands handler

---
+++ internal/transport/tcp/cmd.go
@@ -0,0 +1,75 @@
+package tcp
+
+import (
+	"fmt"
+	"nova/internal/lib/resp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type handler func([]string) []byte
+
+var (
+	cmdInfo = "info"
+	cmdPing = "ping"
+	cmdEcho = "echo"
+	cmdGet  = "get"
+	cmdSet  = "set"
+)
+
+var (
+	ErrUnknownCmd        = "Unknown command"
+	ErrWrongNumberOfArgs = "Wrong number of arguments for '%s' command"
+	ErrSyntax            = "syntax error"
+)
+
+func (h *Handler) pingHandler(args []string) []byte {
+	return resp.EncodeSimpleString("PONG")
+}
+
+func (h *Handler) echoHandler(args []string) []byte {
+	if len(args) != 2 {
+		return resp.EncodeError(fmt.Sprintf(ErrWrongNumberOfArgs, cmdEcho))
+	}
+	return resp.EncodeString(args[1])
+}
+
+func (h *Handler) getHandler(args []string) []byte {
+	if len(args) != 2 {
+		return resp.EncodeError(fmt.Sprintf(ErrWrongNumberOfArgs, cmdGet))
+	}
+	key := args[1]
+	value, ok := h.storage.Get(key)
+	if ok {
+		return resp.EncodeString(value)
+	}
+	return resp.NullString
+}
+
+func (h *Handler) setHandler(args []string) []byte {
+	if len(args) < 3 {
+		return resp.EncodeError(fmt.Sprintf(ErrWrongNumberOfArgs, cmdGet))
+	} else if len(args) > 3 {
+		switch strings.ToLower(args[3]) {
+		case "px":
+			if len(args) > 5 {
+				return resp.EncodeError(ErrSyntax)
+			}
+			ttl, err := strconv.Atoi(args[4])
+			if err != nil {
+				// TODO: should be completely another error. I'll definitely fix it
+				return resp.EncodeError(ErrSyntax)
+			}
+			key, value := args[1], args[2]
+			h.storage.Set(key, value, time.Duration(ttl)*time.Millisecond)
+		default:
+			return resp.EncodeError(ErrSyntax)
+		}
+	} else {
+		key, value := args[1], args[2]
+		h.storage.Set(key, value, 0)
+	}
+
+	return resp.EncodeSimpleString("OK")
+}
---
+++ internal/transport/tcp/handler.go
@@ -0,0 +1,42 @@
+package tcp
+
+import (
+	"log/slog"
+	"strings"
+	"time"
+)
+
+type Storage interface {
+	Set(key, value string, ttl time.Duration)
+	Get(key string) (string, bool)
+}
+
+type Handler struct {
+	log     slog.Logger
+	storage Storage
+	dict    map[string]handler
+}
+
+func NewHandler(log slog.Logger, storage Storage) *Handler {
+	h := &Handler{
+		log:     log,
+		storage: storage,
+	}
+
+	dict := map[string]handler{
+		cmdPing: h.pingHandler,
+		cmdEcho: h.echoHandler,
+
+        cmdGet: h.getHandler,
+        cmdSet: h.setHandler,
+	}
+
+	h.dict = dict
+	return h
+}
+
+func (h *Handler) Serve(args []string) []byte {
+	cmd := strings.ToLower(args[0])
+
+	return h.dict[cmd](args)
+}
