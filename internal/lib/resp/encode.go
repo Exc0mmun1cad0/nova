e95dc1e6c0e28346fce108cb4c1f3a3c8070cb96
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-08-16 15:32:01 +0300

    feat: add resp encoding for main types

---
+++ internal/lib/resp/encode.go
@@ -0,0 +1,41 @@
+package resp
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+var (
+	NullString = []byte("$-1\r\n")
+)
+
+func EncodeSimpleString(str string) []byte {
+	res := fmt.Sprintf("+%s\r\n", str)
+	return []byte(res)
+}
+
+func EncodeError(errMsg string) []byte {
+	res := fmt.Sprintf("-%s\r\n", errMsg)
+	return []byte(res)
+}
+
+func EncodeString(str string) []byte {
+	res := fmt.Sprintf("$%d\r\n%s\r\n", len(str), str)
+	return []byte(res)
+}
+
+// TODO: it doesn't work with INFO command when iredis starts
+func EncodeArray(strs []string) []byte {
+	var b bytes.Buffer
+
+	b.WriteByte('*')
+	b.WriteString(strconv.Itoa(len(strs)))
+	b.WriteString("\r\n")
+
+	for _, str := range strs {
+		b.Write(EncodeString(str))
+	}
+
+	return b.Bytes()
+}
