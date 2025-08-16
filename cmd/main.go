a31d1c09b86e14e7f9386e85e496b88f5260c0f7
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-08-16 22:50:47 +0300

    feat: entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"log"
-	"net"
+	"context"
+	"log/slog"
+	"nova/internal/handler"
+	mapstorage "nova/internal/storage/map"
+	"nova/internal/tcp"
+	"os"
 )
 
 // TODO: move to configuration module (or package)
@@ -14,35 +15,16 @@ var (
 )
 
 func main() {
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("failed to init socket listener: %v", err.Error())
+	storage := mapstorage.New(context.Background())
+
+	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	handler := handler.NewHandler(log, storage)
+
+	srv := &tcp.Server{
+		Addr:    addr,
+		Handler: handler,
 	}
 
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Printf("failed to accept client connection: %v", err.Error())
-		}
-
-		go handleConn(conn)
-	}
-}
-
-func handleConn(conn net.Conn) {
-	for {
-		buff := make([]byte, 100)
-		n, err := conn.Read(buff)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				fmt.Println("connection closed. Maybe...")
-				break
-			}
-
-			fmt.Printf("failed to read request due to: %v", err.Error())
-		}
-
-		req := buff[:n]
-		fmt.Printf("Got request: %s\n", string(req))
-	}
+	srv.ListenAndServe()
 }
